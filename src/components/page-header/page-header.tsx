967ae84673a78c0f8f87279386eac3a19e35ac32
ArinaKo <arinakovalenco7@gmail.com>
2023-07-24 16:24:02 +0300

    Выносить шапку страницы в отдельный компонент

---
+++ src/components/page-header/page-header.tsx
@@ -0,0 +1,30 @@
+import PageNav from '../page-nav/page-nav';
+
+type PageHeaderProps = {
+  isNavActive?: boolean;
+};
+
+function PageHeader({ isNavActive = true }: PageHeaderProps): JSX.Element {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <a className="header__logo-link header__logo-link--active">
+              <img
+                className="header__logo"
+                src="img/logo.svg"
+                alt="6 cities logo"
+                width={81}
+                height={41}
+              />
+            </a>
+          </div>
+          {isNavActive && <PageNav />}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default PageHeader;
