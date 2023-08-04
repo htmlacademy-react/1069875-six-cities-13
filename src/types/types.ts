ba82148a79a399819dd989d41d61b74faef7ffdd
ArinaKo <arinakovalenco7@gmail.com>
2023-08-04 21:43:22 +0300

    Добавляет моки отзывов

--- src/types/types.ts
+++ src/types/types.ts
@@ -24,14 +24,23 @@ type OfferAdditionT = {
   description: string;
   bedrooms: number;
   goods: string[];
-  host: {
-    name: string;
-    avatarUrl: string;
-    isPro: boolean;
-  };
+  host: UserT;
   images: string[];
   maxAdults: number;
 };
 
 export type OfferFullT = Omit<OfferT, 'previewImage'> & OfferAdditionT;
 
+type UserT = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+};
+
+export type ReviewT = {
+  id: string;
+  date: string;
+  user: UserT;
+  comment: string;
+  rating: number;
+};
