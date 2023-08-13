3aa1a0c69963d20ec9016bfe3f3aa4c34297e189
ArinaKo <arinakovalenco7@gmail.com>
2023-08-13 16:39:34 +0300

    Дополняет компонент главной страницы

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { useState } from 'react';
 import PageHeader from '../../components/page-header/page-header';
 import LocationTabs from '../../components/location-tabs/location-tabs';
@@ -11,7 +12,7 @@ import { changeCity, getCityOffers } from '../../store/action';
 function MainPage(): JSX.Element {
   const activeCity = useAppSelector((state) => state.city);
   const offers = useAppSelector((state) => state.cityOffers);
-  const [activeOffer, setActiveOffer] = useState<null|string>(null);
+  const [activeOffer, setActiveOffer] = useState<null | string>(null);
 
   const dispatch = useAppDispatch();
 
@@ -22,49 +23,80 @@ function MainPage(): JSX.Element {
 
   return (
     <div className="page page--gray page--main">
-      <PageHeader isMainPage/>
-      <main className="page__main page__main--index">
+      <PageHeader isMainPage />
+      <main className={cn('page__main page__main--index', {'page__main--index-empty': !offers.length})}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
-          <LocationTabs activeCity={activeCity} onClick={handleLocationTabClick}/>
+          <LocationTabs
+            activeCity={activeCity}
+            onClick={handleLocationTabClick}
+          />
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offers.length} places to stay in {activeCity}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
+          {offers.length ? (
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">
+                  {offers.length} places to stay in {activeCity}
+                </b>
+                <form className="places__sorting" action="#" method="get">
+                  <span className="places__sorting-caption">Sort by</span>
+                  <span className="places__sorting-type" tabIndex={0}>
                     Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
-              <OffersList mode={OffersListMode.All} offers={offers} onMouseMove={setActiveOffer}/>
-            </section>
-            <div className="cities__right-section">
-              <Map mode={MapMode.MainPage} city={CityLocation[activeCity]} activePoint={activeOffer} points={offers.map((offer) => ({...offer.location, id: offer.id}))} />
+                    <svg className="places__sorting-arrow" width={7} height={4}>
+                      <use xlinkHref="#icon-arrow-select" />
+                    </svg>
+                  </span>
+                  <ul className="places__options places__options--custom places__options--opened">
+                    <li
+                      className="places__option places__option--active"
+                      tabIndex={0}
+                    >
+                      Popular
+                    </li>
+                    <li className="places__option" tabIndex={0}>
+                      Price: low to high
+                    </li>
+                    <li className="places__option" tabIndex={0}>
+                      Price: high to low
+                    </li>
+                    <li className="places__option" tabIndex={0}>
+                      Top rated first
+                    </li>
+                  </ul>
+                </form>
+                <OffersList
+                  mode={OffersListMode.All}
+                  offers={offers}
+                  onMouseMove={setActiveOffer}
+                />
+              </section>
+              <div className="cities__right-section">
+                <Map
+                  mode={MapMode.MainPage}
+                  city={CityLocation[activeCity]}
+                  activePoint={activeOffer}
+                  points={offers.map((offer) => ({
+                    ...offer.location,
+                    id: offer.id,
+                  }))}
+                />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">
+                    We could not find any property available at the moment in {activeCity}
+                  </p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>
+          )}
         </div>
       </main>
     </div>
