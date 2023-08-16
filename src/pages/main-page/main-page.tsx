adbd1b6b7694cfa0d511f4e61c39aad44cd22bc8
ArinaKo <arinakovalenco7@gmail.com>
2023-08-16 21:27:05 +0300

    Подключает компонент сортировки

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -4,21 +4,25 @@ import PageHeader from '../../components/page-header/page-header';
 import LocationTabs from '../../components/location-tabs/location-tabs';
 import OffersList from '../../components/offers-list/offers-list';
 import Map from '../../components/map/map';
+import Sorting from '../../components/sorting/sorting';
 import { City, CityLocation } from '../../const/cities';
 import { MapMode, OffersListMode } from '../../const/modes';
+import { SortingType } from '../../const/others';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { changeCity } from '../../store/action';
-import { getOffersByCity } from '../../scripts/offers';
+import { getOffersByCity, getOffersSorted } from '../../scripts/offers';
 
 function MainPage(): JSX.Element {
   const activeCity = useAppSelector((state) => state.city);
   const offers = getOffersByCity(useAppSelector((state) => state.offers), activeCity);
   const [activeOffer, setActiveOffer] = useState<null | string>(null);
+  const [activeSorting, setActiveSorting] = useState<typeof SortingType[keyof typeof SortingType]>(SortingType.Default);
 
   const dispatch = useAppDispatch();
 
   const handleLocationTabClick = (city: typeof City[keyof typeof City]) => {
     dispatch(changeCity({ city }));
+    setActiveSorting(SortingType.Default);
   };
 
   return (
@@ -40,35 +44,10 @@ function MainPage(): JSX.Element {
                 <b className="places__found">
                   {offers.length} places to stay in {activeCity}
                 </b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by</span>
-                  <span className="places__sorting-type" tabIndex={0}>
-                    Popular
-                    <svg className="places__sorting-arrow" width={7} height={4}>
-                      <use xlinkHref="#icon-arrow-select" />
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li
-                      className="places__option places__option--active"
-                      tabIndex={0}
-                    >
-                      Popular
-                    </li>
-                    <li className="places__option" tabIndex={0}>
-                      Price: low to high
-                    </li>
-                    <li className="places__option" tabIndex={0}>
-                      Price: high to low
-                    </li>
-                    <li className="places__option" tabIndex={0}>
-                      Top rated first
-                    </li>
-                  </ul>
-                </form>
+                <Sorting activeSorting={activeSorting} onClick={setActiveSorting}/>
                 <OffersList
                   mode={OffersListMode.All}
-                  offers={offers}
+                  offers={getOffersSorted(offers, activeSorting)}
                   onMouseMove={setActiveOffer}
                 />
               </section>
