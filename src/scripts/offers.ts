840dfe966496a5afade4ba84f6c3491c4248d14b
ArinaKo <arinakovalenco7@gmail.com>
2023-08-16 21:22:46 +0300

    Добавляет функции фильтрации предложений

--- src/scripts/offers.ts
+++ src/scripts/offers.ts
@@ -1,11 +1,16 @@
 import { OfferT } from '../types/types';
 import { City } from '../const/cities';
+import { SortingFunction } from './sorting';
 
 export const getOffersByCity = (
   offers: OfferT[],
   city: typeof City[keyof typeof City]
 ): OfferT[] => offers.filter((offer) => offer.city.name === city);
 
-export const getOffersByFavor = (
-  offers: OfferT[]
-): OfferT[] => offers.filter(({ isFavorite }) => isFavorite);
+export const getOffersByFavor = (offers: OfferT[]): OfferT[] =>
+  offers.filter(({ isFavorite }) => isFavorite);
+
+export const getOffersSorted = (offers: OfferT[], sorting: string): OfferT[] =>
+  SortingFunction[sorting]
+    ? [...offers].sort(SortingFunction[sorting])
+    : offers;
