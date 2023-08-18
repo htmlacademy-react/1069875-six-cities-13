37e8c20a74e837db7aa32e2afab24680b381147f
ArinaKo <arinakovalenco7@gmail.com>
2023-08-18 21:13:55 +0300

    Добавляет обработку ошибок ответа сервера

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,32 @@
-import axios, { AxiosInstance} from 'axios';
+import axios, { AxiosError, AxiosInstance} from 'axios';
+import { StatusCodes } from 'http-status-codes';
 import { BACKEND_URL, REQUEST_TIMEOUT } from '../const/server';
+import { toast } from 'react-toastify';
 
-export const createAPI = (): AxiosInstance => (
-  axios.create({
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+type DetailMessageType = {
+  type: string;
+  message: string;
+}
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
-  }));
+  });
+
+  api.interceptors.response.use((response) => response, (error: AxiosError<DetailMessageType>) => {
+    if (error.response && StatusCodeMapping[error.response.status]) {
+      const detailMessage = (error.response.data);
+      toast.warn(detailMessage.message);
+    }
+    throw error;
+  });
+
+  return api;
+};
