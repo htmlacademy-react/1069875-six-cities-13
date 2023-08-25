3000aa498a9bc9979babd3870a0f06507150e52e
ArinaKo <arinakovalenco7@gmail.com>
2023-08-25 18:04:07 +0300

    Оптимизирует компонент offers-list

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -3,43 +3,61 @@ import OfferCard from '../offer-card/offer-card';
 import { OfferT } from '../../types/offer';
 import OffersListModeDiffs from './offers-list-mode-diffs';
 import { OffersListMode } from '../../const/modes';
+import { useCallback, useMemo } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { setActiveOffer } from '../../store/main-data/main-data';
+import { getCurrentSorting } from '../../store/main-data/selectors';
+import { getOffersSorted } from '../../utils/offers';
 
 type OffersListProps = {
   mode: typeof OffersListMode[keyof typeof OffersListMode];
   offers: OfferT[];
-  onMouseMove?: (id: string|null) => void;
+  isInteractive: boolean;
 };
 
 function OffersList({
   mode,
   offers,
-  onMouseMove,
+  isInteractive = false,
 }: OffersListProps): JSX.Element {
   const { StyleClass } = OffersListModeDiffs[mode];
+  const activeSorting = useAppSelector(getCurrentSorting);
+  const dispatch = useAppDispatch();
+
+  const sortedOffers = useMemo(() => getOffersSorted(offers, activeSorting), [offers, activeSorting]);
+
+  const handleMouseMove = useCallback((id: string | null) => {
+    dispatch(setActiveOffer(id));
+  }, [dispatch]);
+
   return (
     <div
       className={
         cn(
           `places__list ${StyleClass}`,
-          {'tabs__content': onMouseMove},
+          {'tabs__content': isInteractive},
         )
       }
       onMouseOut={
-        onMouseMove &&
-        (() => {
-          onMouseMove(null);
-        })
+        isInteractive ?
+          (() => {
+            handleMouseMove(null);
+          })
+          :
+          undefined
       }
     >
-      {offers.map((offer) => (
+      {sortedOffers.map((offer) => (
         <OfferCard
           key={offer.id}
           offer={offer}
           onMouseOver={
-            onMouseMove &&
-            (() => {
-              onMouseMove(offer.id);
-            })
+            isInteractive ?
+              (() => {
+                handleMouseMove(offer.id);
+              })
+              :
+              undefined
           }
         />
       ))}
